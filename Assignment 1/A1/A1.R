8da3147db50405ea3c9a5c8cee31ac34779b79c7
kaylaipp <kaylaipp@bu.edu>
2018-01-22 19:43:18 -0500

    added print statements to .r document for easier grading and so result prints on pdf as well

--- Assignment 1/A1/A1.R
+++ Assignment 1/A1/A1.R
@@ -23,10 +23,12 @@ MyData <- read.csv(file="/Users/kaylaippongi/Desktop/read_this_1.csv", sep = "")
   f <- rep(tmp, len = 31)
   g <- rep(tmp, c(10,20,30))
   output<-list(a,b,c,e,f,g)
+  print(output)
 
 #Problem 2
 x <- seq(from = 3.0, to = 6.0, length.out = 30) 
 h <- exp(x)*cos(x)
+print(h)
 
 #Problem 3
 #Part a
@@ -35,20 +37,22 @@ i <- rep(x, times = 12)
 j <- c(3,1,6,4,9,7,12,10,15,12,18,16,21,19,
        24,22,27,25,30,28,33,31,36,34)
 result <- i^j
+print(result)
 
 #Part b
-x <- c(2)
-numerator <- c(x, times = 25)
 denominator <- c(1:25)
-result <- ((numerator)^numerator/denominator)
+result <- (((2)^denominator)/denominator)
+print(result)
 
 #Problem 4
 #Part a
 i <- c(10:100)
 result <- sum(i^3 + 4*(i^2))
+print(result)
 #Part b
 j <- c(1:25)
 result2 <- sum(( (2^j)/j) + (3^j)/(j^2) )
+print(result)
 
 #Problem 5
 #Part a
@@ -63,34 +67,48 @@ xVect <- sample(0:999, 250, replace=T)
 yVect <- sample(0:999, 250, replace=T)
 #Part a
 result_a <- yVect - xVect
+print(result_a)
 #Part b
 result_b <- sin(yVect)/cos(xVect)
+print(result_b)
 #Part c
 result_c <- xVect + 2*xVect - xVect
+print(result_c)
 #Part d
 sum( (exp(-xVect+1))/(xVect +10))
+print(sum( (exp(-xVect+1))/(xVect +10)))
+
 
 #Problem 7
 #Part a
 result_7a <- xVect[(xVect>600)]
+print(result_7a)
 #Part b
 result_7b <- which(yVect>600)
+print(result_7b)
 #Part c 
 result_7c <- xVect[which(yVect>600)]
+print(result_7c)
 #Part d
 result_7d <-c(abs(xVect-mean(xVect))^0.5)
+print(result_7d)
 #Part e
 result_7e <-which(yVect<(min(200)))
-#Part f
+print(result_7e)
+#Part f 
 result_7f <- sum(1-xVect%%2)
+print(result_7f)
 #Part g
 result_7g <- sort(order(yVect)[xVect]) 
+print(result_7g)
 #Part h
 indexes <-seq(from=1, to=250, by = 3)
 result_7h <-yVect[indexes]
+print(result_7h)
 
 #Problem 8
 num <- seq(from = 2, to= 38, by =2)
 denom <- seq(from =3, to = 39, by =2)
-cumprod(num/denom)
+print(1+sum(cumprod(num/denom)))
+
 
